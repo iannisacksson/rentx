94dc996b6b7444b2cc64e2a5f730a85309a4406e
Iann Isacksson <iisackssonoliveira@gmail.com>
2022-01-08 18:12:38 -0300

    feat: Devolution rental route

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express';
 
 import { CreateRentalController } from '@modules/rentals/useCases/createRentals/CreateRentalController';
+import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
 
 import { ensureAutheticated } from '../middlewares/ensureAutheticated';
 
 const rentalsRoutes = Router();
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 rentalsRoutes.use(ensureAutheticated);
 
 rentalsRoutes.post('/', createRentalController.handle);
+
+rentalsRoutes.post('/:id/devolution', devolutionRentalController.handle);
 
 export { rentalsRoutes };
