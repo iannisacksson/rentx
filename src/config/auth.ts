cc262e28ff9f6ae3b4531cb71cd770a1a276f132
Iann Isacksson <iisackssonoliveira@gmail.com>
2022-01-09 10:31:09 -0300

    chore: Add auth config

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: 'asjfhcaenqrvpiubenvpiujufnbvfhjvdb',
+  secret_refresh_token: 'c1a1dff670ba33e2575582df07f29569',
+  expires_in_token: '15m',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+};
