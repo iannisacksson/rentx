f579615ba93c0c0d7bab373c09962e52e1c21243
Iann Isacksson <iisackssonoliveira@gmail.com>
2022-01-09 11:43:06 -0300

    feat: Send forgot password email controller

---
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { SendForgotPasswordMailUseCase } from './SendForgotPasswordMailUseCase';
+
+class SendForgotPasswordMailController {
+  public async handle(request: Request, response: Response): Promise<Response> {
+    const { email } = request.body;
+
+    const sendForgotPasswordMailUseCase = container.resolve(
+      SendForgotPasswordMailUseCase,
+    );
+
+    await sendForgotPasswordMailUseCase.execute({ email });
+
+    return response.status(204).send();
+  }
+}
+
+export { SendForgotPasswordMailController };
